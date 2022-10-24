dfe2aac127aac88d205aa9bc27141bbb4c02ce93
YOHANNES KANKO <johanneskanko7@gmail.com>
2022-10-24 04:03:40 -0700

    task 7

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,16 +1,42 @@
 #!/usr/bin/python3
-"""The Basegeometry module"""
+"""
+This is module 7-base_geometry
+
+This module contains:
+-one class
+-two functions
+
+Access test for this class in /tests subfolder - to run:
+`python3 -m doctest ./tests/7-base_geometry.txt`
+"""
 
 
 class BaseGeometry:
-    """Basegeometry class"""
-    def __init__(self):
-        pass
+    """
+    base geometry class
+
+    Public instance method:
+    `def area(self)`
+    `def integer_validator(self, name, value)`
+
+    Args:
+    name
+    value
+    Returns:
+    area of shape
+    """
 
     def area(self):
+        """
+        public method to determine area
+        Raises:
+        Exception with the message
+        """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """validates value from int type
+        """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
