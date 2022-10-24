5e5c76bc383eff5b3c8f1ec1cc664e2c833816cf
YOHANNES KANKO <johanneskanko7@gmail.com>
2022-10-24 03:46:15 -0700

    Update

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,33 @@
 #!/usr/bin/python3
-"""The mylist module"""
+"""
+This is module 1-my_list
+
+This module contains one class and one public instance method
+
+Access test for this class in /tests subfolder - to run:
+`python 3 -m doctest ./tests/1-my_list.txt`
+"""
 
 
 class MyList(list):
-    """My list class"""
+    """
+    inherits from `list`
+
+    Public instance method:
+    `def print_sorted(self):`
+
+    Args:
+    list
+
+    Returns:
+    prints to stdout list sorted by ascending order
+    """
+
     def print_sorted(self):
-        i = self.copy()
-        i.sort()
-        print(i)
+        """
+        prints the list, but sorted (ascending sort)
+        """
+        sorted_list = MyList()
+        for item in self:
+            sorted_list.append(item)
+        print(sorted(sorted_list))
