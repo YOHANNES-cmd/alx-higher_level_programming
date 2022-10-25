bc51fecde6d82bebf7465e87a7770bdc6994b2b7
YOHANNES KANKO <johanneskanko7@gmail.com>
2022-10-25 05:30:23 -0700

    task 7 update

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,16 +1,32 @@
 #!/usr/bin/python3
+"""The to json string module"""
+import json
+from sys import argv
+from os.path import exists
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-"""Add all arguments to a Python list and save them to a file."""
-import sys
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
+lp = []
+if exists("add_item.json"):
+    with open("add_item.json", "r", encoding="Utf-8") as file:
+        lines = file.readlines()
+        for i in lines:
+            if i != "":
+                lp = json.loads(i)
+else:
+    with open("add_item.json", "w", encoding="Utf-8") as file:
+        pass
 
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+with open("add_item.json", "r", encoding="Utf-8") as file:
+    lines = file.readlines()
+    for i in lines:
+        if i != "":
+            lp = json.loads(i)
+
+for i in range(1, len(argv)):
+    lp.append(argv[i])
+
+with open("add_item.json", "w", encoding="Utf-8") as file:
+    lpo = "[" + ", ".join(lp) + "]"
+    file.write(json.dumps(lp))
