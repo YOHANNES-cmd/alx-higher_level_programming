65ebfa86cc8fd8c80c64ab28e80300fe9d9df8a1
YOHANNES KANKO <johanneskanko7@gmail.com>
2022-11-28 23:41:24 -0800

    task 101 nad 102

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,3 @@
+-- Displays the top 3 of cities temperature during July and August ordered
+-- by temperature (descending)
+SELECT city, avg(value) AS avg_temp FROM temperatures WHERE month >= 7 AND month <= 8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3
