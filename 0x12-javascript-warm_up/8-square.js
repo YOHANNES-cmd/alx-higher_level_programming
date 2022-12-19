1048ec35eee67453ab34c3df8a596d37f8f911a6
YOHANNES KANKO <johanneskanko7@gmail.com>
2022-12-19 03:03:10 -0800

    8. Square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,21 +1,20 @@
 #!/usr/bin/node
-
 /**
- * square - prints a square with 'X'.
- * @param {Nunmber} size - the of the square.
+ * square - Prints a square with 'X'.
+ * @param {Number} size - The size of the square.
  */
 function square (size) {
-	if (Number.isNaN(size)) {
-		console.log('Missing size');
-	} else if (size >= 0){
-		const row = new Array(size);
-		for (let i = 0; i < size; i++){
-			row.push('X');
-		}
-		let rows = new Array(size);
-		rows = rows.fill(row.join(''), 0, size);
-		console.log(rows.join('\n'));
-	}
+  if (Number.isNaN(size)) {
+    console.log('Missing size');
+  } else if (size >= 0) {
+    const row = new Array(size);
+    for (let i = 0; i < size; i++) {
+      row.push('X');
+    }
+    let rows = new Array(size);
+    rows = rows.fill(row.join(''), 0, size);
+    console.log(rows.join('\n'));
+  }
 }
 
 square(Number.parseInt(process.argv[2]));
