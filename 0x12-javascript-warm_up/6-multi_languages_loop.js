ee06b86cfa83fd97ef8b54d6c30776bd004742f4
YOHANNES KANKO <johanneskanko7@gmail.com>
2022-12-19 02:47:34 -0800

    6. Loop to languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
-const texts = ['C is fun','Python is cool','JavaScript is amazing'];
+const texts = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
 
-for (let i = 0; i < texts.length; i++){
-	console.log(texts[i]);
+for (let i = 0; i < texts.length; i++) {
+  console.log(texts[i]);
 }
