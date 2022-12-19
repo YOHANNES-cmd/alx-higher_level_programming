1cdffcd78fadb889aec62846020a55eec1bac579
YOHANNES KANKO <johanneskanko7@gmail.com>
2022-12-19 03:24:19 -0800

    13. Add file

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+module.exports = {
+  /**
+   * add - Computes the sum of 2 numbers.
+   * @param {Number} a - The first number.
+   * @param {Number} b - The second number.
+   *
+   * @returns The sum of the 2 numbers.
+   */
+  add: (a, b) => a + b
+};
