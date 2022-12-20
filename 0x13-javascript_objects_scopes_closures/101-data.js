98341191e4772f3da9577ca24b40833680b51a59
YOHANNES KANKO <johanneskanko7@gmail.com>
2022-12-20 00:47:11 -0800

    12. Sorted occurences

---
+++ 0x13-javascript_objects_scopes_closures/101-data.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/**
+ * An arbitrary object with keys as occurrences and their
+ * corresponding value as a user id.
+ */
+exports.dict = {
+  89: 1,
+  90: 2,
+  91: 1,
+  92: 3,
+  93: 1,
+  94: 2
+};
