dfc826592551937e0e009af142e9deb5f543abf5
yohannes kanko <johanneskanko7@gmail.com>
2023-01-16 23:43:15 -0800

    0. What's my status? #0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Fetches a https://alx-intranet.hbtn.io/status"""
+import urllib.request as request
+
+if __name__== "__main__":
+    with request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+        if response.readable():
+            data = response.read()
+            print("Body response:")
+            print("\t- type: {}".format(type(data)))
+            print("\t- content: {}".format(data))
+            print("\t- utf8 content: {}".format(data.decode("utf-8")))
