8ea86306756a93569c88bcda022bc21d6a4e1868
yohannes kanko <johanneskanko7@gmail.com>
2023-01-17 00:15:29 -0800

    4. What's my status? #1

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Python script that fetches an URL with requests package
+"""
+import requests
+
+if __name__ == '__main__':
+    response = requests.get('https://intranet.hbtn.io/status')
+    print('Body response:')
+    print('\t- type: {}'.format(type(response.text)))
+    print('\t- content: {}'.format(response.text))
