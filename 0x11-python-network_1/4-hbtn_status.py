f5f6ba0d2d515fadfcd8d19e59d666502c24a93a
yohannes kanko <johanneskanko7@gmail.com>
2023-01-17 03:06:45 -0800

    4. What's my status? #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""Python script that fetches an URL with requests package"""
+"""
+Python script that fetches an URL with requests package
+"""
 import requests
 
 
-if __name__ == '__main__':
-    response = requests.get('https://intranet.hbtn.io/status')
-    print('Body response:')
-    print('\t- type: {}'.format(type(response.text)))
-    print('\t- content: {}'.format(response.text))
+if __name__ == "__main__":
+    r = requests.get('https://alx-intranet.hbtn.io/status')
+    t = r.text
+    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(t), t))
